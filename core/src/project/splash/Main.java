b21bdd976c06727ed3ed6296f15d6015d9ca7642
Dennis Chau <d7chau@gmail.com>
2018-09-24 00:00:28 -0400

    Trying to create energy loss for ball

--- core/src/project/splash/Main.java
+++ core/src/project/splash/Main.java
@@ -2,87 +2,72 @@ package project.splash;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public class Main extends ApplicationAdapter implements InputProcessor {
+public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
-    TextureAtlas textureAtlas;
-    SpriteSheet basketballNet;
-    TextureRegion textureRegion;
-    Sprite bballNet;
-    private BitmapFont font;
-    int currentFrame = 1;
-    int maxFrames = 10;
-	
+	Texture txtball;
+	Sprite sprBall;
+	int nBallX = 100, nBallY = 200, nBallWidth = 100, nBallHeight = 100, nCountJump = 0;
+	double dGravity = 0.5, dFallSpeed = 0, dJumpSpeed = 20;
+	boolean bCanFall = true, bCanJump = true;
+
 	@Override
 	public void create () {
-        batch = new SpriteBatch();
-        textureAtlas = new TextureAtlas(Gdx.files.internal("core/assets/SpriteSheet/NetSpriteSheet.atlas"));
-        textureRegion = textureAtlas.findRegion("Net1");
-        bballNet = new Sprite(textureRegion);
-        bballNet.setPosition(100, 100);
-        //basketballNet = new SpriteSheet (textureAtlas.findRegion("Net1"), 100, 100, 144, 156);
-        Gdx.input.setInputProcessor(this);
-        font = new BitmapFont();
-        font.setColor(Color.BLACK);
-    }
+		batch = new SpriteBatch();
+		txtball = new Texture("basketball.png");
+		sprBall = new Sprite(txtball);
+	}
 
-    @Override
+	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-        bballNet.draw(batch);
+		batch.draw(txtball, nBallX, nBallY, nBallWidth, nBallHeight); //ball
 		batch.end();
+		HandleJumping();
+		HandleFalling();
 	}
 
-    @Override
+	public void HandleJumping() {
+		if (bCanJump) {
+			nCountJump++;
+			nBallY += dJumpSpeed;
+			dJumpSpeed -= dGravity;
+
+			if (nCountJump >= 40) {
+				bCanJump = false;
+				bCanFall = true;
+				dJumpSpeed = 20;
+				nCountJump = 0;
+			}
+		}
+	}
+
+	public void HandleFalling() {
+		if (bCanFall) {
+			nBallY -= dFallSpeed;
+			dFallSpeed += dGravity;
+		}
+		if (nBallY <= 100) {
+			bCanJump = true;
+			bCanFall = false;
+     		dFallSpeed = 0;
+		}
+
+//		if (nBallY > 100) {    //trying to create energy loss in the ball everytime it touches ground
+//			dFallSpeed *= -1;
+//		} else dFallSpeed += dGravity;
+	}
+
+
+	@Override
 	public void dispose () {
 		batch.dispose();
+		txtball.dispose();
 	}
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
-    }
 }
