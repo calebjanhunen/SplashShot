dd8cfc7e27d972f064703d11d8a85cbb87f7c350
Dennis Chau <d7chau@gmail.com>
2018-10-09 22:27:19 -0400

    .

--- core/src/project/splash/ScrPlay.java
+++ core/src/project/splash/ScrPlay.java
@@ -6,25 +6,25 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class ScrPlay implements Screen {
 	GamMain game;
 	SpriteBatch batch;
 	Texture txtball;
 	Sprite sprBall;
-	int nBallX = 100, nBallY = 200, nBallWidth = 100, nBallHeight = 100, nCountJump = 0;
-	double dGravity = 0.5, dFallSpeed = 0, dJumpSpeed = 20;
-	boolean bCanFall = true, bCanJump = true;
-//	Vector balllocation, ballvelocity, ballgravity;
+	Vector2 balllocation, ballvelocity, ballgravity;
+	int nBallWidth = 100, nBallHeight = 100;
 
 	public ScrPlay(GamMain game) {
 		this.game = game;
 		batch = new SpriteBatch();
 		txtball = new Texture("basketball.png");
 		sprBall = new Sprite(txtball);
-		//balllocation = new Vector(300,0);
-//		ballvelocity = new Vector(1,2);
-//		ballgravity = new Vector(0,1);
+		balllocation = new Vector2(300,300);
+		ballvelocity = new Vector2((float)8.0,(float)10.0);
+		ballgravity = new Vector2(0,(float) 0.5);
+
 	}
 
 	@Override
@@ -32,43 +32,22 @@ public class ScrPlay implements Screen {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(txtball, nBallX, nBallY, nBallWidth, nBallHeight); //ball
+		batch.draw(txtball, balllocation.x, balllocation.y, nBallWidth, nBallHeight); //ball
 		batch.end();
 
-//		HandleJumping();
-//		HandleFalling();
+		HandleBouncing();
 	}
 
-//	public void HandleJumping() {
-//		if (bCanJump) {
-//			nCountJump++;
-//			nBallY += dJumpSpeed;
-//			dJumpSpeed -= dGravity;
-//
-//			if (nCountJump >= 40) {
-//				bCanJump = false;
-//				bCanFall = true;
-//				dJumpSpeed = 20;
-//				nCountJump = 0;
-//			}
-//		}
-//	}
+	public void HandleBouncing(){
+		balllocation.y += ballvelocity.y;  //  https://www.openprocessing.org/sketch/67284#
+		ballvelocity.y -= ballgravity.y;
 
-//	public void HandleFalling() {
-//		if (bCanFall) {
-//			nBallY -= dFallSpeed;
-//			dFallSpeed += dGravity;
-//		}
-//		if (nBallY <= 0) {
-//			bCanJump = true;
-//			bCanFall = false;
-//			dFallSpeed = 0;
-//			dJumpSpeed = dJumpSpeed * -0.95;
-//			dFallSpeed = dFallSpeed * -0.95;
-//			nBallY = 0;
-//			System.out.println(dJumpSpeed + ", " + dFallSpeed);
-//		}
-//	}
+		if (balllocation.y < 0) {
+			ballvelocity.y = (float)(ballvelocity.y * -0.9);
+			balllocation.y = 0;
+		}
+	}
+
     @Override
 	public void show () {
 
