1fb5c680481a4f9c02fb5522f477171f4c2b15db
Dennis Chau <d7chau@gmail.com>
2018-10-09 20:31:17 -0400

    .

--- core/src/project/splash/ScrPlay.java
+++ core/src/project/splash/ScrPlay.java
@@ -28,7 +28,7 @@ public class ScrPlay implements Screen {
 	}
 
 	@Override
-	public void render () {
+	public void render(float delta) {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
@@ -69,9 +69,29 @@ public class ScrPlay implements Screen {
 //			System.out.println(dJumpSpeed + ", " + dFallSpeed);
 //		}
 //	}
-
+    @Override
 	public void show () {
-		return;
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
 	}
 
 	@Override
--- desktop/src/project/splash/desktop/DesktopLauncher.java
+++ desktop/src/project/splash/desktop/DesktopLauncher.java
@@ -2,12 +2,12 @@ package project.splash.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import project.splash.Main;
+import project.splash.GamMain;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+		new LwjglApplication(new GamMain(), config);
 		config.height = 1000;
 		config.width = 600;
 	}
