e3fa1c710b5427b77452fcfe1aab4773a33a6666
Caleb Janhunen <calebjanhunen@gmail.com>
2018-10-22 13:09:53 -0400

    working on sprNet

--- core/src/project/splash/ScrPlay.java
+++ core/src/project/splash/ScrPlay.java
@@ -9,20 +9,21 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.
+import project.splash.sprNet;
 
-//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public class ScrPlay extends ApplicationAdapter implements InputProcessor, Screen {
+public class ScrPlay implements InputProcessor, Screen {
     GamMain game;
 	SpriteBatch batch;
     sprNet basketballNet;
     TextureRegion textureRegion;
     Sprite arSprNet[] = new Sprite[10];
-    sprNet arSprNet2;
     private BitmapFont font;
     OrthographicCamera camera;
     int nY, nY2, nDy, iSpr, nX, nX2, nDx;
     Texture tx;
+    sprNet net = new sprNet();
 
     public ScrPlay (GamMain game){
         this.game = game;
@@ -55,8 +56,11 @@ public class ScrPlay extends ApplicationAdapter implements InputProcessor, Scree
         //arSprNet[iSpr].draw(batch);
         //arSprNet2.draw(batch);
 //        }
+
+
         batch.end();
-        System.out.println(nDy + "    " + nDx + "    " + iSpr);
+        net.update();
+      //  System.out.println(nDy + "    " + nDx + "    " + iSpr);
     }
 
     @Override
@@ -101,57 +105,57 @@ public class ScrPlay extends ApplicationAdapter implements InputProcessor, Scree
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        nY = Gdx.input.getY();
-        nX = Gdx.input.getX();
+//        nY = Gdx.input.getY();
+//        nX = Gdx.input.getX();
         return true;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        iSpr = 0;
+        //iSpr = 0;
         //nDx = 0;
         return true;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        // net y direction
-        nY2 = Gdx.input.getY();
-        nDy = nY2 - nY;
-        if (nDy < 29){
-            iSpr = 0;
-        }else if (nDy >= 29 && nDy < 48){
-            iSpr = 1;
-        }else if (nDy >= 49 && nDy < 68){
-            iSpr = 2;
-        }else if (nDy >= 69 && nDy < 88){
-            iSpr = 3;
-        }else if (nDy >= 89 && nDy < 108){
-            iSpr = 4;
-        }else if (nDy >= 109 && nDy < 128){
-            iSpr = 5;
-        }else if (nDy >= 129 && nDy < 148){
-            iSpr = 6;
-        }else if (nDy >= 149 && nDy < 168){
-            iSpr = 7;
-        }else if (nDy >= 169 && nDy < 188){
-            iSpr = 8;
-        }else if (nDy >= 189 && nDy < 208){
-            iSpr = 9;
-        } else if (nDy >= 208){
-            iSpr = 9;
-        } else if (nDy >= 208){
-            iSpr = 9;
-        }
-
-        //net x direction
-        nX2 = Gdx.input.getX();
-        nDx = nX2 - nX;
-        if (nDx >= 90){
-            nDx = 90;
-        } else if (nDx <= -90){
-            nDx = -90;
-        }
+//        // net y direction
+//        nY2 = Gdx.input.getY();
+//        nDy = nY2 - nY;
+//        if (nDy < 29){
+//            iSpr = 0;
+//        }else if (nDy >= 29 && nDy < 48){
+//            iSpr = 1;
+//        }else if (nDy >= 49 && nDy < 68){
+//            iSpr = 2;
+//        }else if (nDy >= 69 && nDy < 88){
+//            iSpr = 3;
+//        }else if (nDy >= 89 && nDy < 108){
+//            iSpr = 4;
+//        }else if (nDy >= 109 && nDy < 128){
+//            iSpr = 5;
+//        }else if (nDy >= 129 && nDy < 148){
+//            iSpr = 6;
+//        }else if (nDy >= 149 && nDy < 168){
+//            iSpr = 7;
+//        }else if (nDy >= 169 && nDy < 188){
+//            iSpr = 8;
+//        }else if (nDy >= 189 && nDy < 208){
+//            iSpr = 9;
+//        } else if (nDy >= 208){
+//            iSpr = 9;
+//        } else if (nDy >= 208){
+//            iSpr = 9;
+//        }
+//
+//        //net x direction
+//        nX2 = Gdx.input.getX();
+//        nDx = nX2 - nX;
+//        if (nDx >= 90){
+//            nDx = 90;
+//        } else if (nDx <= -90){
+//            nDx = -90;
+//        }
         return true;
     }
 
