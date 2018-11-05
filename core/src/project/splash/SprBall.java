a815f8d026a3415739747f0bd3e337544f7f0fc1
Dennis Chau <d7chau@gmail.com>
2018-11-04 23:15:06 -0500

    .

--- core/src/project/splash/SprBall.java
+++ core/src/project/splash/SprBall.java
@@ -8,13 +8,17 @@ public class SprBall extends Sprite {
 
     Vector2 balllocation, ballvelocity, ballgravity;
     Texture txtball, txt;
+    Sprite SprBall = new Sprite();
 
-    public SprBall(Texture _txt, int _nX2, int _nY2) {
+
+    public SprBall(Texture _txt, int _nX2, int _nY2, int _nW2, int _nH2) {
         this.txt = _txt;
         txtball = new Texture("basketball.png");
-        balllocation = new Vector2(_nX2,_nY2);
+        balllocation = new Vector2(100, 100);
         ballvelocity = new Vector2((float)8.0,(float)10.0);
         ballgravity = new Vector2(0,(float) 0.5);
+        SprBall.setOrigin(_nX2, _nY2 );
+        SprBall.setSize(_nW2, _nH2);
     }
 
     public float update(){
@@ -25,12 +29,8 @@ public class SprBall extends Sprite {
             ballvelocity.y = (float)(ballvelocity.y * -0.9);
             balllocation.y = 0;
         }
+
         return balllocation.y;
     }
-
-    public void Yvelocity (){
-
-    }
-
 
 }
