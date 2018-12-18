0e7e2ac53a9b0e2ae86257f677e519685b969da1
Dennis Chau <34014458+d7chau@users.noreply.github.com>
2018-12-17 23:39:29 -0500

    .

--- core/src/project/splash/Screens/ScrPlay.java
+++ core/src/project/splash/Screens/ScrPlay.java
@@ -16,7 +16,6 @@ import com.badlogic.gdx.math.Vector2;
 import project.splash.GamMain;
 import project.splash.Objects.SprBall;
 import project.splash.Objects.SprNet;
-
 import java.util.Random;
 
 
@@ -91,7 +90,7 @@ public class ScrPlay implements Screen, InputProcessor {
     }
 
     @Override
-    public void render(float delta) {           //RENDERRRRRR
+    public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
