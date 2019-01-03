ea59b092c2971728300a6def05ab3cbca61f4419
Dennis Chau <d7chau@gmail.com>
2019-01-02 22:40:12 -0500

    .

--- core/src/project/splash/Objects/Buttons.java
+++ core/src/project/splash/Objects/Buttons.java
@@ -1,13 +1,10 @@
-package gdx.game;
+package project.splash.Objects;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
-/**
- *
- * @author Dennis
- */
+
 public class Buttons extends Sprite { //Learning how to implement buttons: https://github.com/brauj1894/Risk/blob/master/core/src/gdx/game/objects/Button.java
     int nX, nY, nW, nH;
 
